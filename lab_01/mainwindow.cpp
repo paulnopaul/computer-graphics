f8f172e1067d438708fb7fd37ba3ce8c186f7ac0
Paul <tscheklin@gmail.com>
2020-02-15 15:16:44 +0300

    Update

---
+++ lab_01/mainwindow.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Павел Чеклин on 15/02/2020.
+//
+
+#include "mainwindow.h"
---
+++ lab_01/mainwindow.h
@@ -0,0 +1,14 @@
+//
+// Created by Павел Чеклин on 15/02/2020.
+//
+
+#ifndef LAB_01_MAINWINDOW_H
+#define LAB_01_MAINWINDOW_H
+
+
+class mainwindow {
+
+};
+
+
+#endif //LAB_01_MAINWINDOW_H
